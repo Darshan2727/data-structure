07d233ccff265f98f48a9bfc46cc2b324f0edeb5
darshan <darshan2701k@gmail.com>
2023-03-11 09:20:00 +0530

    ...

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define n 15
 void insertion(int a[])
 {
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=i+1;j<n;j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if(a[i]>a[j])
+            if (a[i] > a[j])
             {
-                int temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
             }
         }
     }
@@ -20,14 +20,13 @@ int main()
 {
     int a[n];
     printf("Unsorted array:");
-    for(int i=0;i<n;i++)
-    a[i]=rand() % 100;
-    for(int i=0;i<n;i++)
-    printf(" %d",a[i]);
+    for (int i = 0; i < n; i++)
+        a[i] = rand() % 100;
+    for (int i = 0; i < n; i++)
+        printf(" %d", a[i]);
 
     insertion(a);
     printf("\nSorted array with Insertion sort:");
-    for(int i=0;i<n;i++)
-    printf(" %d", a[i]);
-
+    for (int i = 0; i < n; i++)
+        printf(" %d", a[i]);
 }
