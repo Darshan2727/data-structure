baf11ee79defd18e3241b613555154d46aa7b076
darshan <darshan2701k@gmail.com>
2023-03-13 09:58:44 +0530

    ...

---
+++ queue.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#define n 6
+int a[n];
+int f=-1,r=-1,i;
+int dis()
+{
+    if(f<0)
+        printf("Array is Empty1");
+    else
+    {
+        for(i=f;i<=r;i++)
+            printf(" %d",a[i]);
+    }    
+    printf("\n");
+}
+int insertlast(int val)
+{
+    if(r>=n-1)
+        printf("Array is Full\n");
+    else if(r<0)
+    {
+        f=r=0;
+        a[r]=val;
+    } 
+    else
+    {
+        a[++r]=val;
+    }   
+}
+int delete()
+{
+    if(f==-1 || f>r)
+    {
+        printf("Array if EMPTY\n");
+        return;
+    }
+    else if(f<0)
+        f=r=-1;
+    else
+    {
+       
+       f++;
+    }  
+}
+int main()
+{
+    insertlast(10);
+    insertlast(20);
+    insertlast(30);
+    insertlast(40);
+    insertlast(50);
+    delete();
+    delete();
+    delete();
+    delete();
+    delete();
+  
+   
+   
+    dis();
+    
+}
