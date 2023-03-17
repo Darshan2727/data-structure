de18b2e61ba7cecb71bf6e3c03f974c5ce7eb4f4
darshan <darshan2701k@gmail.com>
2023-03-17 09:27:32 +0530

    ...

--- binary-recursive.c
+++ binary-recursive.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #define n 10
-int binarysearch(int a[], int l,int h,int x)
+int binarysearch(int a[], int l, int h, int x)
 {
-    // int l = 0, h = n - 1, mid;
-    if(h>=l)
+    if (h >= l)
     {
-        int mid = l + (h-l) / 2;
+        int mid = l + (h - l) / 2;
         if (a[mid] == x)
-            return mid;
-        if (a[mid] > x)
-            return binarysearch(a,l,mid-1,x);
-        // else       
-        //     h = mid - 1;
-        return binarysearch(a,mid+1,h,x);
+            return mid + 1;
+        else if (a[mid] > x)
+            return binarysearch(a, l, mid - 1, x);
+        else
+            return binarysearch(a, mid + 1, h, x);
     }
     return -1;
 }
@@ -22,14 +20,14 @@ int main()
     int a[n] = {5, 9, 13, 16, 22, 25, 28, 31, 35, 36};
     for (int i = 0; i < n; i++)
         printf(" %d", a[i]);
-    int x,l,h;
+    int x, l, h;
     printf("\nEnter Search Element:");
     scanf("%d", &x);
 
-    int b = binarysearch(a,l,n-1,x);
+    int b = binarysearch(a, 0, n - 1, x);
     if (b == -1)
         printf("Element is NOT FOUND...");
     else
         printf("Element FOUND at %d position...", b);
- return 0;    
+    return 0;
 }
--- recursive.c
+++ recursive.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 int fun(int i)
 {
-    printf("\nBefore Inside function %d ",i);
-    if(i<3)
+    printf("\nBefore Inside function %d ", i);
+    if (i < 3)
         fun(++i);
     else
         return 0;
-    printf("\nAfter Inside Function %d ",i);        
+    printf("\nAfter Inside Function %d ", i);
 }
 int main()
 {
