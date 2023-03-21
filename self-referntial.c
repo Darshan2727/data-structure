40c8f3bdfd1bf812ad1f094617686dc63f61db53
darshan <darshan2701k@gmail.com>
2023-03-21 09:29:32 +0530

    ...

---
+++ self-referntial.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+struct self
+{
+    int n;
+    char c;
+    struct self *ptr;
+};
+int main()
+{
+    struct self a, b, d, e;
+
+    a.n = 70;
+    a.c = 'J';
+    a.ptr = NULL;
+
+    b.n = 77;
+    b.c = 'H';
+    b.ptr = NULL;
+
+    d.n = 99;
+    d.c = 'A';
+    d.ptr = NULL;
+
+    e.n = 23;
+    e.c = 'Z';
+    e.ptr = NULL;
+
+    // printf("Before Using pointer");
+    // printf("\nA -> value of n:%d\tch:%c",a.n,a.c);
+    // printf("\nB -> value of n:%d\tch:%c",b.n,b.c);
+
+    b.ptr = &a;
+    a.ptr = &b;
+    e.ptr = &d;
+    d.ptr = &e;
+
+    printf("\n\nAfter Using pointer");
+    printf("\nA -> value of n:%d\tch:%c", b.ptr->n, b.ptr->c);
+    printf("\nB -> value of n:%d\tch:%c", a.ptr->n, a.ptr->c);
+    printf("\nD -> value of n:%d\tch:%c", e.ptr->n, e.ptr->c);
+    printf("\nE -> value of n:%d\tch:%c", d.ptr->n, d.ptr->c);
+}
