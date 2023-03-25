4f465fed589e4bfbd2e8452b18a511382beaf9d3
darshan <darshan2701k@gmail.com>
2023-03-25 08:31:00 +0530

    ...

--- malloc.c
+++ malloc.c
@@ -20,17 +20,17 @@ int display()
 }
 void firstinsert(int aa)
 {
-    struct node*ptr =head;
+    struct node *ptr = head;
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     temp->data = aa;
 
-    if(head!=NULL)
+    if (head != NULL)
     {
-        temp->next=head;
-        head=temp;
+        temp->next = head;
+        head = temp;
         return;
     }
-}    
+}
 void insertend(int val)
 {
     struct node *ptr = head;
@@ -52,20 +52,20 @@ void insertend(int val)
     }
     return;
 }
-void insertmid(int bb,int pos)
+void insertmid(int bb, int pos)
 {
     struct node *ptr = head;
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     temp->data = bb;
     //  temp->data = pos;
     struct node *prev;
-        while (ptr->data != pos)
-        {
-            prev=ptr;
-            ptr=ptr->next;
-        }
-        prev->next=temp;
-        temp->next=ptr;
+    while (ptr->data != pos)
+    {
+        prev = ptr;
+        ptr = ptr->next;
+    }
+    prev->next = temp;
+    temp->next = ptr;
     return;
 }
 void firstdelete()
@@ -79,8 +79,8 @@ void firstdelete()
     {
         head = NULL;
         free(ptr);
-    }    
-    else 
+    }
+    else
     {
         head = ptr->next;
         free(ptr);
@@ -92,14 +92,14 @@ void middelete(int pos)
     struct node *ptr = head;
     struct node *prev;
 
-        while (ptr->data != pos)
-        {
-            prev = ptr;
-            ptr = ptr->next;
-        }
-        prev->next = ptr->next;
-        free(ptr);
-    
+    while (ptr->data != pos)
+    {
+        prev = ptr;
+        ptr = ptr->next;
+    }
+    prev->next = ptr->next;
+    free(ptr);
+
     return;
 }
 void delete()
@@ -128,16 +128,63 @@ void delete()
 }
 int main()
 {
-    insertend(10);
-    insertend(20);
-    insertend(30);
-    insertend(40);
-    insertend(50);
-    insertend(60);
-    display();
-    // delete ();
-    // delete ();
-    // insertmid(99,30);
-    middelete(30);
-    display();
+    int ch, x, y;
+    printf("1.First Insert:");
+    printf("\n2.Mid Insert:");
+    printf("\n3.Last Insert:");
+    printf("\n4.First Delete:");
+    printf("\n5.Mid Delete:");
+    printf("\n6.Last Delete:");
+    printf("\n9.Display:");
+    printf("\n0.Exit....");
+    while (ch != 0)
+    {
+        printf("\n Enter Your Choice:");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            printf("Enter Element:");
+            scanf("%d", &x);
+            firstinsert(x);
+            break;
+
+        case 2:
+            printf("Enter Element And Data:");
+            scanf("%d %d", &x, &y);
+            insertmid(x, y);
+            break;
+
+        case 3:
+            printf("Enter Element:");
+            scanf("%d", &x);
+            insertend(x);
+            break;
+
+        case 4:
+            firstdelete();
+            break;
+
+        case 5:
+            printf("Enter Data:");
+            scanf("%d", &x);
+            middelete(x);
+            break;
+
+        case 6:
+            delete ();
+            break;
+
+        case 9:
+            display();
+            break;
+
+        case 0:
+            break;
+
+        default:
+            printf("\n WRONG choice:");
+        }
+    }
 }
